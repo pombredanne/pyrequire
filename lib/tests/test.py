3f52fe25880bfe83c8520a959c3e351d0073d0f9
Riley Denoyer <radbuglet@gmail.com>
2016-07-02 06:44:11 -0700

    Updated tests

--- lib/tests/test.py
+++ lib/tests/test.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 import sys
 
-def exports(uppercase, lowercase, capitalize):
-    return {
-        "uppercase": uppercase.upper(),
-        "lowercase": lowercase.lower(),
-        "capitalize": capitalize.capitalize()
-    }
+def exports(cond):
+    return True;
